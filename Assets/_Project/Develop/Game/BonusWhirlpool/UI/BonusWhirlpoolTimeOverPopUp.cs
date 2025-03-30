4e26fc358fd354c11a167e862a252c3f2ed703b6
Roman784 <rmironenko50@gmail.com>
2025-03-30 16:19:17 +0300

    Preventing sudden exit from the bonus whirlpool

--- Assets/_Project/Develop/Game/BonusWhirlpool/UI/BonusWhirlpoolTimeOverPopUp.cs
+++ Assets/_Project/Develop/Game/BonusWhirlpool/UI/BonusWhirlpoolTimeOverPopUp.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using GameplayRoot;
 using GameRoot;
 using System.Collections;
@@ -11,23 +12,34 @@ namespace UI
     public class BonusWhirlpoolTimeOverPopUp : PopUp
     {
         private GameplayEnterParams _gameplayEnterParams;
+        private bool _canExit;
 
         public void SetGameplayEnterParams(GameplayEnterParams gameplayEnterParams)
         {
             _gameplayEnterParams = gameplayEnterParams;
         }
 
-        public void BackToLevel()
+        public override void Open(bool fadeScreen = true)
         {
-            var enterParams = new GameplayEnterParams(_gameplayEnterParams.LevelNumber, 
-                                                      _gameplayEnterParams.BonusWhirlpoolTimerValue);
-            new SceneLoader().LoadAndRunGameplay(enterParams);
+            base.Open(fadeScreen);
+            DOVirtual.DelayedCall(1.5f, () => _canExit = true);
         }
 
         public override void Close(bool appearScreen = true)
         {
+            if (!_canExit) return;
+
             base.Close(appearScreen);
             BackToLevel();
+        }
+
+        public void BackToLevel()
+        {
+            if (!_canExit) return;
+
+            var enterParams = new GameplayEnterParams(_gameplayEnterParams.LevelNumber, 
+                                                      _gameplayEnterParams.BonusWhirlpoolTimerValue);
+            new SceneLoader().LoadAndRunGameplay(enterParams);
         }
 
         public class Factory : PopUpFactory<BonusWhirlpoolTimeOverPopUp>
